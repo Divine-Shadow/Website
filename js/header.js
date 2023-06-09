6894435e96b93f73492bd6a4e2ae98495de3f0a8
Bill <bill@themolchans.com>
2023-06-09 14:10:23 -0400

    ChatGPT help

---
+++ js/header.js
@@ -0,0 +1,28 @@
+class HeaderNav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav class="navbar dark navbar-fixed-top">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="ti-menu"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                  <img src="img/logo-update.png">
+                </a>
+              </div>
+              <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="javascript:void(0)" id="bookbutton" onclick="book()">Booking</a></li>
+                </ul>
+              </div><!--/.nav-collapse -->
+            </div>
+            </nav>
+        `;
+    }
+}
+
+customElements.define("header-nav", HeaderNav);
