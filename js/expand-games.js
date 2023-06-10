770fb3f2603d4abfeaa61234eaf4b7b65e8f00a9
Bill <bill@themolchans.com>
2023-06-09 23:38:46 -0400

    Fixed 'our games' through hack and commented out links to dead sections.

--- js/expand-games.js
+++ js/expand-games.js
@@ -1,9 +1,14 @@
-console.log('Script is running');
-const expandButton = document.querySelector('#expandButton');
-const fullList = document.querySelector('#fullList');
 
-expandButton.addEventListener('click', function() {
+    function handleExpandButtonClick() {
     console.log('Button clicked');
+    const fullList = document.querySelector('#fullList');
     fullList.classList.toggle('expandable');
     fullList.classList.toggle('expanded');
-});
+}
+
+    window.onload = function() {
+    console.log('Script is running');
+    const expandButton = document.querySelector('#expandButton');
+    expandButton.onclick = handleExpandButtonClick;
+};
+
